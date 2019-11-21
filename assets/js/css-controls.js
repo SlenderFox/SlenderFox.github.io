16c1d9150d2b4d6f358928ac795cfc55d3869e88
SlenderFox <SlenderFox@outlook.com>
2019-11-21 15:14:12 +1100

    I think Im learning

--- assets/js/css-controls.js
+++ assets/js/css-controls.js
@@ -1,14 +1,16 @@
-// Removes the scrollbar
-// const block = document.getElementById('MainContainer');
-// block.style.paddingRight = block.offsetWidth - block.clientWidth + "px";
+// A switch that enables and disables light theme
+const button = document.querySelector('input[type="checkbox"]');
 
-const button = document.querySelector('input');
 button.addEventListener('click', function() {
     if (button.value === "enabled") {
+        button.value = "disabled";
         // Light theme enabled
         console.log("Light theme selected");
+        document.querySelector('html').style.backgroundColor = "rgb(255, 255, 255)";
     } else {
+        button.value = "enabled";
         // Dark theme enabled
         console.log("Dark theme selected");
+        document.querySelector('html').style.backgroundColor = "rgb(12, 13, 14)";
     }
 })
