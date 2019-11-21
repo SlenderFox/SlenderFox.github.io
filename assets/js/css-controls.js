29613fdffca50aaa02ba13e3cc57ebd5944103ad
SlenderFox <SlenderFox@outlook.com>
2019-11-21 15:33:07 +1100

    That didn't work but I know why, this might work

--- assets/js/css-controls.js
+++ assets/js/css-controls.js
@@ -1,24 +1,24 @@
 // A switch that enables and disables light theme
 const button = document.querySelector('input[type="checkbox"]');
-const htmlColour = document.querySelector('html').style.backgroundColor;
-const bodyColour = document.querySelector('body').style.color;
-const h1Colour = document.querySelector('h1').style.color;
-const h2Colour = document.querySelector('h2').style.color;
-const h3Colour = document.querySelector('h3').style.color;
-const h4Colour = document.querySelector('h4').style.color;
+const html = document.querySelector('html').style;
+const body = document.querySelector('body').style;
+const h1 = document.querySelector('h1').style;
+const h2 = document.querySelector('h2').style;
+const h3 = document.querySelector('h3').style;
+const h4 = document.querySelector('h4').style;
 
 button.addEventListener('click', function() {
     if (button.value === "enabled") {
         button.value = "disabled";
         console.log("Light theme enabled");
         // Colours changed
-        htmlColour = "rgb(255, 255, 255)";
-        bodyColour, h1Colour, h2Colour, h3Colour, h4Colour = "rbg(5, 5, 5)";
+        html.backgroundColor = "rgb(255, 255, 255)";
+        body.color, h1.color, h2.color, h3.color, h4.color = "rbg(5, 5, 5)";
     } else {
         button.value = "enabled";
         console.log("Light theme disabled");
         // Colours changed
-        htmlColour = "rgb(12, 13, 14)";
-        bodyColour, h1Colour, h2Colour, h3Colour, h4Colour = "rgb(230, 230, 230)";
+        html.backgroundColor = "rgb(12, 13, 14)";
+        body.color, h1.color, h2.color, h3.color, h4.color = "rgb(230, 230, 230)";
     }
 })
