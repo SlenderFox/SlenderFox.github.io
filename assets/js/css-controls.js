df55fe36396473b6c7e5ef102856db13f8a2ed3f
SlenderFox <SlenderFox@outlook.com>
2019-11-21 15:01:12 +1100

    Maybe this will work better

--- assets/js/css-controls.js
+++ assets/js/css-controls.js
@@ -1,18 +1,14 @@
-// This should remove the scrollbar
-const block = document.getElementById('MainContainer');
-block.style.paddingRight = block.offsetWidth - block.clientWidth + "px";
-
-const html = document.querySelector('html');
+// Removes the scrollbar
+// const block = document.getElementById('MainContainer');
+// block.style.paddingRight = block.offsetWidth - block.clientWidth + "px";
 
 const button = document.querySelector('input');
-button.addEventListener("switch", function() {
+button.addEventListener('click', function() {
     if (button.value === "enabled") {
         // Light theme enabled
         console.log("Light theme selected");
-        html.style.backgroundColor = "rgb(255, 255, 255)";
     } else {
         // Dark theme enabled
         console.log("Dark theme selected");
-        html.style.backgroundColor = "rgb(55, 55, 55)";
     }
 })
