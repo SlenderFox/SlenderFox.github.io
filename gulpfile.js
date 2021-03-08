4f7745ca3ca1f257bd4855a43ad76e11da0e1be6
SlenderFox <SlenderFox@outlook.com>
2021-03-08 22:24:07 +1100

    Testing a vscode suggestion

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var cp = require('child_process');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync');
+import { task, series, src, dest, watch } from 'gulp';
+import csso from 'gulp-csso';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import sass from 'gulp-sass';
+import plumber from 'gulp-plumber';
+import { spawn } from 'child_process';
+import imagemin from 'gulp-imagemin';
+import browserSync, { reload } from 'browser-sync';
 
 var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
 
@@ -14,23 +14,23 @@ var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
  */
-gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
+task('jekyll-build', function (done) {
+	return spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
 		.on('close', done);
 });
 
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', gulp.series(['jekyll-build'], function (done) {
-	browserSync.reload();
+task('jekyll-rebuild', series(['jekyll-build'], function (done) {
+	reload();
 	done();
 }));
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', gulp.series(['jekyll-build'], function(done) {
+task('browser-sync', series(['jekyll-build'], function(done) {
 	browserSync({
 		server: {
 			baseDir: '_site'
@@ -42,50 +42,50 @@ gulp.task('browser-sync', gulp.series(['jekyll-build'], function(done) {
 /*
 * Compile and minify sass
 */
-gulp.task('sass', function() {
-  return gulp.src('src/styles/**/*.scss')
+task('sass', function() {
+  return src('src/styles/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(csso())
-		.pipe(gulp.dest('assets/css/'))
+		.pipe(dest('assets/css/'))
 });
 
 /*
 * Compile fonts
 */
-gulp.task('fonts', function() {
-	return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+task('fonts', function() {
+	return src('src/fonts/**/*.{ttf,woff,woff2}')
 		.pipe(plumber())
-		.pipe(gulp.dest('assets/fonts/'))
+		.pipe(dest('assets/fonts/'))
 });
 
 /*
  * Minify images
  */
-gulp.task('imagemin', function() {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
+task('imagemin', function() {
+	return src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'))
+		.pipe(dest('assets/img/'))
 });
 
 /**
  * Compile and minify js
  */
-gulp.task('js', function() {
-	return gulp.src('src/js/**/*.js')
+task('js', function() {
+	return src('src/js/**/*.js')
 		.pipe(plumber())
 		.pipe(concat('main.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
+		.pipe(dest('assets/js/'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/styles/**/*.scss', gulp.series(['sass', 'jekyll-rebuild']));
-  gulp.watch('src/js/**/*.js', gulp.series(['js', 'jekyll-rebuild']));
-  gulp.watch('src/fonts/**/*.{tff,woff,woff2}', gulp.series(['fonts']));
-  gulp.watch('src/img/**/*.{jpg,png,gif}', gulp.series(['imagemin']));
-  gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], gulp.series(['jekyll-rebuild']));
+task('watch', function() {
+  watch('src/styles/**/*.scss', series(['sass', 'jekyll-rebuild']));
+  watch('src/js/**/*.js', series(['js', 'jekyll-rebuild']));
+  watch('src/fonts/**/*.{tff,woff,woff2}', series(['fonts']));
+  watch('src/img/**/*.{jpg,png,gif}', series(['imagemin']));
+  watch(['*html', '_includes/*html', '_layouts/*.html'], series(['jekyll-rebuild']));
 });
 
-gulp.task('default', gulp.series(['js', 'sass', 'fonts', 'browser-sync', 'watch']));
+task('default', series(['js', 'sass', 'fonts', 'browser-sync', 'watch']));
